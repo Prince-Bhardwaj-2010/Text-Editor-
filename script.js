ad38e852a8d3efdfe48ca0d032fb9055cace66f4
Prince-Bhardwaj-2010 <146417639+Prince-Bhardwaj-2010@users.noreply.github.com>
2024-06-13 17:28:56 +1000

    Add files via upload

---
+++ script.js
@@ -0,0 +1,34 @@
+function f1() {
+    document.getElementById("textarea1").style.fontWeight = "Bold"
+}
+function f2() {
+    document.getElementById("textarea1").style.fontStyle = "Italic"
+}
+function f3() {
+    document.getElementById("textarea1").style.textAlign = "left"
+}
+function f4() {
+    document.getElementById("textarea1").style.textAlign = "center"
+}
+function f5() {
+    document.getElementById("textarea1").style.textAlign = "right"
+}
+function f6() {
+    document.getElementById("textarea1").style.textTransform = "Uppercase"
+}
+function f7() {
+    document.getElementById("textarea1").style.textTransform = "Lowercase"
+}
+function f8() {
+    document.getElementById("textarea1").style.textTransform = "Capitalize"
+}
+function f9() {
+    document.getElementById("textarea1").value = " ";
+}
+function f10() {
+       document.getElementById("textarea1").style.fontWeight = "normal";
+          document.getElementById("textarea1").style.textAlign = "left";
+          document.getElementById("textarea1").style.fontStyle = "normal";
+         
+          document.getElementById("textarea1").style.textTransform = "Capitalize";
+}
